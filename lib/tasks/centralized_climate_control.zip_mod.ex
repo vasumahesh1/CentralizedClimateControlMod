c8e1beeb91735044d11c5b921c4d2c0205037f4f
Vasu Mahesh <vasu.mahesh@hotmail.com>
2017-07-19 18:17:58 +0530

    fix: build script

--- lib/tasks/centralized_climate_control.zip_mod.ex
+++ lib/tasks/centralized_climate_control.zip_mod.ex
@@ -4,7 +4,7 @@ defmodule Mix.Tasks.ZipMod do
   @shortdoc "Zips the Mod for Upload."
   @release_dir "release/"
   @modname "CentralizedClimateControl"
-  @version "1.1.0"
+  @version "1.2.1"
   @game_version "A17"
 
   def run(_) do
@@ -24,17 +24,29 @@ defmodule Mix.Tasks.ZipMod do
     
     {:ok, file_list} = File.ls(path)
     
+    copy_dest = Enum.join([@release_dir, @modname, "/"])
+
+    :ok = File.mkdir_p(copy_dest)
+
     files = Enum.map(file_list, fn filename -> Path.join(path, filename) end)
         |> Enum.filter(fn(x) -> Enum.member?(reject_list, x) != true end)
         |> Enum.map(fn(x) -> String.split(x, "./") |> Enum.at(1) end)
         |> Enum.map(&String.to_charlist/1)
 
-    IO.puts "Following Directories will be put in the Mod:"
+    IO.puts "\nFollowing Directories will be put in the Mod:"
     IO.inspect files
 
-    filename = Enum.join([@release_dir, "[", @game_version, "]", "[", @version, "]", @modname , ".zip"]) |> String.to_charlist
-    :zip.create(filename, files)
+    IO.puts ["\nMoving Files to: ", copy_dest]
+    Enum.each(files, &File.cp_r(&1, Enum.join([copy_dest, &1])))
 
-    IO.puts ["Created File: ", filename]
+    dest_zip_files = Enum.map(files, &Enum.join([@modname, "/", &1])) |> Enum.map(&String.to_charlist/1)
+    
+    IO.puts "\nFollowing Directories will be Zipped:"
+    IO.inspect dest_zip_files
+
+    filename = Enum.join([@release_dir, "[", @game_version, "]", "[", @version, "]", @modname , ".zip"]) |> String.to_charlist
+    IO.puts ["\nCreating File: ", filename]
+
+    {:ok, _} = :zip.create(filename, dest_zip_files, [{:cwd, String.to_charlist(@release_dir) }])
   end
 end
